cd2716c1235ef7d764bac9944a7c28ea28d81b6d
Samantha Bhuiyan <sbhuiyan802@hotmail.com>
2015-11-18 18:28:09 -0800

    added ifC

--- src/assignment3/main.go
+++ src/assignment3/main.go
@@ -14,6 +14,10 @@ type NumV struct {
 	i int
 }
 
+type BoolV struct {
+    b bool
+}
+
 func (n NumV) String() string {
 	return fmt.Sprint(n.i)
 }
@@ -32,15 +36,43 @@ type BinC struct {
 	R ExprC
 }
 
+type ifC struct {
+    B bool
+    X ExprC
+    Y ExprC
+}
+
+func (i ifC) Interp() Value {
+    if i.B == true {
+        return i.X.Interp()
+    }
+
+    return i.Y.Interp()
+}
+
 func (b BinC) Interp() Value {
-	vL := b.L.Interp()
-	vR := b.R.Interp()
-	return NumV{vL.(NumV).i + vR.(NumV).i}
+    vL := b.L.Interp()
+    vR := b.R.Interp()
+    return NumV{vL.(NumV).i + vR.(NumV).i}
 }
 
 func main() {
 	b := &BinC{"+", NumC{1}, NumC{2}}
-	fmt.Println(b)
-	fmt.Println(NumC{1}.Interp())
+
+    c := &ifC{true, NumC{1}, NumC{2}}
+    d := &ifC{false, NumC{1}, NumC{2}}
+
+    fmt.Println(b)
+    fmt.Println(b.Interp())
+
+    fmt.Println(c)
+    fmt.Println(c.Interp())
+
+    fmt.Println(d)
+    fmt.Println(d.Interp())
+
 }
+
+
+
 
